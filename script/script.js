c7d6bea5186a60cbbe5573b943d5e8c5c65cbff5
Yorí <yorigcsdev@gmail.com>
2022-04-13 21:08:28 -0300

    Carrega chat

--- script/script.js
+++ script/script.js
@@ -1,11 +1,47 @@
 function loadMessage() {
     const load = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
 
-    load.then(ms);
+    load.then(loading);
 }
 
-function ms(data) {
-    console.log(data);
+function loading(data) {
+    
+    const dataUsers = data.data;
+    let renderUsers = document.querySelector('main');
+    renderUsers.innerHTML = '';
+
+    for(let i =0; i < dataUsers.length; i++) {
+        if(dataUsers[i].type === 'status') {
+            renderUsers.innerHTML += `
+            <div class="status">
+            <span class="time">(${dataUsers[i].time})</span>
+            <span class="from">${dataUsers[i].from}</span>
+            <span class="text">${dataUsers[i].text}</span>
+            </div>
+            `;
+        } else if (dataUsers[i].type === 'message') {
+            renderUsers.innerHTML += `
+            <div class="message">
+            <span class="time">(${dataUsers[i].time})</span>
+            <span class="from">${dataUsers[i].from}</span>
+            <span>para</span>
+            <span class="to">${dataUsers[i].to}:</span>
+            <span class="text">${dataUsers[i].text}</span>
+            </div>
+            `;
+        } else {
+            renderUsers.innerHTML += `
+            <div class="private_message">
+            <span class="time">(${dataUsers[i].time})</span>
+            <span class="from">${dataUsers[i].from}</span>
+            <span>para</span>
+            <span class="to">${dataUsers[i].to}:</span>
+            <span class="text">${dataUsers[i].text}</span>
+            </div>
+            `;
+        }
+        
+    }
 }
 
-loadMessage();
+setInterval(loadMessage,5000);
